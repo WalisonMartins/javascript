31c3648476dcf3b60214e3827d5cafb12e317c9c
Walison Martins <martinswlaves@gmail.com>
2023-04-12 23:59:20 -0300

    Promise - Error handling

--- notes/functions/Promise.js
+++ notes/functions/Promise.js
@@ -10,4 +10,35 @@ function gerarNumerosEntre(min, max, tempo) {
   })
 }
 
-//Para gerar 10 números de forma aleatória de "uma vez só"
+//Para gerar 5 números de forma aleatória de "uma vez só"
+function gerarVariosNumeros() {
+  return Promise.all([
+    gerarNumerosEntre(1, 60, 1000),
+    gerarNumerosEntre(1, 60, 500),
+    gerarNumerosEntre(1, 60, 1000),
+    gerarNumerosEntre(1, 60, 100),
+    gerarNumerosEntre(1, 60, 300),
+  ])
+}
+
+gerarVariosNumeros().then(numeros => console.log(numeros))
+
+//Nos tópicos seguintes estão algumas explicações relacionadas a tratamento de erro no Promise
+
+function funcionarOuNao(valor, chanceErro) {
+
+  return new Promise((resolve, reject) => {
+    if (Math.random() < chanceErro) {
+      reject('Ocorreu um erro!')
+    } else {
+      resolve(valor)
+    }
+  })
+}
+
+funcionarOuNao('Testando...', 0.9)
+  .then(v => console.log(`Valor: ${v}`))
+
+  //Posso tratar o erro da seguinte forma, é importante deixar o catch no final para não impactar o encadeamento:
+  .catch(err => console.log(`Erro: ${err}`))
+  .then(() => console.log('Fim!'))
